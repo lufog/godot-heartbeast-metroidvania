9590aba996814d167b9fe30515bffc26b4f7965c
Aleksey Smirnov <debugger94@gmail.com>
2022-08-06 22:39:32 +0300

    Part 48

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-const DUST_EFFECT_SCENE: PackedScene = preload("res://effects/dust_effect/dust_effect.tscn")
-const JUMP_EFFECT_SCENE: PackedScene =preload("res://effects/jump_effect/jump_effect.tscn")
-const BULLET_SCENE: PackedScene = preload("res://player/projectile/player_bullet.tscn")
+const DustEffectScene: PackedScene = preload("res://effects/dust_effect/dust_effect.tscn")
+const JumpEffectScene: PackedScene =preload("res://effects/jump_effect/jump_effect.tscn")
+const BulletScene: PackedScene = preload("res://player/projectile/player_bullet.tscn")
+
+enum { MOVE, WALL_SLIDE }
 
 var stats = LoadedResources.player_stats
 
@@ -14,6 +16,7 @@ var stats = LoadedResources.player_stats
 @export var bullet_speed: float = 250
 @export var invincible := false
 
+var state := MOVE
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping: bool = false
 var double_jump: bool = true
@@ -32,59 +35,68 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	match state:
+		MOVE:
+			if not is_on_floor() and is_on_wall():
+				state = WALL_SLIDE
+				double_jump = true
+			
+			var facing_direction: int = sign(get_local_mouse_position().x)
+			sprite.scale.x = facing_direction
+			
+			var direction := Input.get_axis("move_left", "move_right")
+			if direction:
+				if direction == facing_direction:
+					sprite_animation_player.play("run")
+				else:
+					sprite_animation_player.play_backwards("run")
+				
+				velocity.x += direction * acceleration * delta
+				velocity.x = clamp(velocity.x, -max_speed, max_speed)
+			else:
+				sprite_animation_player.play("idle")
+				velocity.x = lerp(velocity.x, 0, friction)
+			
+			if is_on_floor():
+				is_jumping = false
+				double_jump = true
+			else:
+				# Apply gravity.
+				velocity.y += gravity * delta
+				velocity.y = clamp(velocity.y, -jump_force, jump_force)
+
+				sprite_animation_player.play("jump")
+				if Input.is_action_just_released("jump") and velocity.y < -jump_force / 2:
+					velocity.y = -jump_force / 2
+				
+				if Input.is_action_just_pressed("jump") and double_jump:
+					Utils.instantiate_scene_on_main(JumpEffectScene, global_position)
+					velocity.y = -jump_force * 0.75
+					double_jump = false
+			
+			if is_on_floor() or not coyote_timer.is_stopped():
+				if Input.is_action_just_pressed("jump"):
+					Utils.instantiate_scene_on_main(JumpEffectScene, global_position)
+					velocity.y = -jump_force
+					is_jumping = true
+					coyote_timer.stop()
+			
+			var was_on_flor = is_on_floor()
+
+			move_and_slide()
+
+			if was_on_flor and not is_on_floor() and not is_jumping:
+				coyote_timer.start()
+			
+			if not was_on_flor and is_on_floor():
+				Utils.instantiate_scene_on_main(JumpEffectScene, global_position)
+		
+		WALL_SLIDE:
+			if is_on_floor():
+				state = MOVE
+	
 	if Input.is_action_pressed("fire") and fire_bullet_timer.is_stopped():
 		_fire_bullet()
-	
-	var facing_direction: int = sign(get_local_mouse_position().x)
-	sprite.scale.x = facing_direction
-	
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		if direction == facing_direction:
-			sprite_animation_player.play("run")
-		else:
-			sprite_animation_player.play_backwards("run")
-		
-		velocity.x += direction * acceleration * delta
-		velocity.x = clamp(velocity.x, -max_speed, max_speed)
-	else:
-		sprite_animation_player.play("idle")
-		velocity.x = lerp(velocity.x, 0, friction)
-	
-	if is_on_floor():
-		is_jumping = false
-		double_jump = true
-	else:
-		# Apply gravity.
-		velocity.y += gravity * delta
-		velocity.y = clamp(velocity.y, -jump_force, jump_force)
-
-		sprite_animation_player.play("jump")
-		if Input.is_action_just_released("jump") and velocity.y < -jump_force / 2:
-			velocity.y = -jump_force / 2
-		
-		if Input.is_action_just_pressed("jump") and double_jump:
-			Utils.instantiate_scene_on_main(JUMP_EFFECT_SCENE, global_position)
-			velocity.y = -jump_force * 0.75
-			double_jump = false
-	
-	if is_on_floor() or not coyote_timer.is_stopped():
-		if Input.is_action_just_pressed("jump"):
-			Utils.instantiate_scene_on_main(JUMP_EFFECT_SCENE, global_position)
-			velocity.y = -jump_force
-			is_jumping = true
-			coyote_timer.stop()
-	
-	var was_on_flor = is_on_floor()
-
-	move_and_slide()
-
-	if was_on_flor and not is_on_floor() and not is_jumping:
-		coyote_timer.start()
-	
-	if not was_on_flor and is_on_floor():
-		Utils.instantiate_scene_on_main(JUMP_EFFECT_SCENE, global_position)
-
 
 func _on_hurtbox_hit(damage) -> void:
 	if not invincible:
@@ -99,12 +111,12 @@ func _on_died() -> void:
 
 func _create_dust_effect() -> void:
 	var dust_position := global_position + Vector2(randf_range(-4, 4), 0)
-	Utils.instantiate_scene_on_main(DUST_EFFECT_SCENE, dust_position)
+	Utils.instantiate_scene_on_main(DustEffectScene, dust_position)
 
 
 func _fire_bullet() -> void:
 	fire_bullet_timer.start()
-	var bullet := Utils.instantiate_scene_on_main(BULLET_SCENE, muzzle_location.global_position)
+	var bullet := Utils.instantiate_scene_on_main(BulletScene, muzzle_location.global_position)
 	bullet.velocity = Vector2.RIGHT.rotated(gun.rotation) * bullet_speed
 	bullet.velocity.x *= sprite.scale.x
 	bullet.rotation = bullet.velocity.angle()
