d2f8e565d53672bf6464338b11cb25bf17352545
Aleksey Smirnov <debugger94@gmail.com>
2022-08-21 21:13:54 +0300

    Part 58

---
+++ projectile/player_missle.gd
@@ -0,0 +1,13 @@
+extends Projectile
+
+
+const BRICK_LAYER_BIT := 5
+
+const EnemyDeathEffect: PackedScene = preload("res://effects/enemy_death_effect/enemy_death_effect.tscn")
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body is StaticBody2D and body.get_collision_layer_value(BRICK_LAYER_BIT):
+		body.queue_free()
+		Utils.instantiate_scene_on_main(EnemyDeathEffect, global_position + Vector2(8, 8))
+	super._on_hitbox_body_entered(body)
--- projectile/projectile.gd
+++ projectile/projectile.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name Projectile
 
 
-const EXPLOSION_EFFECT_SCENE: PackedScene = preload("res://effects/explosion_effect/explosion_effect.tscn")
+const ExplosionEffectScene: PackedScene = preload("res://effects/explosion_effect/explosion_effect.tscn")
 
 var velocity := Vector2.ZERO 
 
@@ -17,10 +17,10 @@ func _on_visible_notifier_screen_exited() -> void:
 
 
 func _on_hitbox_body_entered(_body: Node2D) -> void:
-	Utils.instantiate_scene_on_main(EXPLOSION_EFFECT_SCENE, global_position)
+	Utils.instantiate_scene_on_main(ExplosionEffectScene, global_position)
 	queue_free()
 
 
 func _on_hitbox_area_entered(_area: Area2D) -> void:
-	Utils.instantiate_scene_on_main(EXPLOSION_EFFECT_SCENE, global_position)
+	Utils.instantiate_scene_on_main(ExplosionEffectScene, global_position)
 	queue_free()
